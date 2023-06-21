60afd8448ce58a9bb64c2f5da0c4f0f729738675
CholongPak <cholongpak08@gmail.com>
2023-06-21 05:49:02 -0500

    Datatype_String

---
+++ C#_Basic/Datatype_String/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Cholong";
+            string message = "My name is " + myName;
+
+            string capsMessage = message.ToUpper();
+
+            string lowerMessage = message.ToLower();
+            
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowerMessage);
+
+        }
+    }
+}

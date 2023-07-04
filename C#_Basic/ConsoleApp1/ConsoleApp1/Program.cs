f0d022c8de333949c74fdf0c36b53136a18981e9
CholongPak <cholongpak08@gmail.com>
2023-07-03 19:28:32 -0500

    Variables

---
+++ C#_Basic/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95F;
+            var character = 'A';
+            var firstName = "Cholong";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14F;
+            Console.WriteLine(Pi);
+        }
+    }
+}
--- C#_Basic/HelloWorld/HelloWorld/Program.cs
+++ C#_Basic/HelloWorld/HelloWorld/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Welcome");
+            Console.Write("Hello");
+            Console.WriteLine("Welcome");
+
+            Console.Write("Enter a string and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have endtered {0}", readInput);
+
+            Console.Write("Enter a string and press enter: ");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is {0}", asciiValue);
+            Console.ReadKey();
         }
     }
 }
